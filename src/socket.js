016a9657398401a713dd03174e2ca7ff9e38a545
martialrahulsharma <martialrahulsharma@gmail.com>
2025-02-10 23:46:17 +0530

    update socket connection to use config variable and update asset references

--- src/socket.js
+++ src/socket.js
@@ -1,29 +1,30 @@
 import { io } from "socket.io-client";
+import config from "./config";
 
 let socket;
 
 const connectSocket = () => {
-if (!socket) {
-// Initialize socket connection if it doesn’t already exist
-socket = io("http://localhost:3000", { transports: ["websocket"] });
-}else if (!socket.connected) {
-// If socket exists but is disconnected, reconnect it
-socket.connect();
-}
+  if (!socket) {
+    // Initialize socket connection if it doesn’t already exist
+    socket = io(config.VARTA_APP_URL, { transports: ["websocket"] });
+  } else if (!socket.connected) {
+    // If socket exists but is disconnected, reconnect it
+    socket.connect();
+  }
 
-// Optional: Set up a listener to confirm reconnection
-socket.on("connect", () => {
-console.log("Socket connected:", socket.id);
-});
+  // Optional: Set up a listener to confirm reconnection
+  socket.on("connect", () => {
+    console.log("Socket connected:", socket.id);
+  });
 
-return socket;
+  return socket;
 };
 
 const disconnectSocket = () => {
-if (socket) {
-socket.disconnect();
-console.log("Socket disconnected");
-}
+  if (socket) {
+    socket.disconnect();
+    console.log("Socket disconnected");
+  }
 };
 
 export { connectSocket, disconnectSocket };
